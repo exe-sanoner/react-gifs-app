64e13b7caa766f4e8eaaf58dc3bad83756ba31a6
Exe Sanoner <exe.sanoner@gmail.com>
2021-09-28 23:13:20 -0300

    feat: add css-grids + infinite scroll in Searchs & frists tests

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import Spinner from 'components/Spinner/index';
-import ListOfGifs from 'components/ListOfGifs/ListOfGifs';
-import {useGifs} from 'hooks/useGifs';
+import React, { useCallback, useEffect, useRef } from "react";
+import Spinner from "components/Spinner/index";
+import ListOfGifs from "components/ListOfGifs/ListOfGifs";
+import { useGifs } from "hooks/useGifs";
+import userNearScreen from "hooks/useNearScreen";
+import debounce from "just-debounce-it";
 
-export default function SearchResults ({ params }) {
-    // console.log(params);
-    const { keyword } = params
-    const { loading, gifs, setPage } = useGifs({ keyword });   // custom hook
+export default function SearchResults({ params }) {
+  // console.log(params);
+  const { keyword } = params;
+  const { loading, gifs, setPage } = useGifs({ keyword }); // custom hook
+  const externalRef = useRef();
+  const { isNearScreen } = userNearScreen({
+    externalRef: loading ? null : externalRef,
+    once: false,
+  });
 
-    const handleNextPage = () => setPage(prevPage => prevPage + 1)
+  // handleNextPage del BUTTON
+  // const handleNextPage = () => setPage(prevPage => prevPage + 1)
 
-    return <>
-        {loading
-            ? <Spinner />
-            : <>
-                <h3 className="App-title">{decodeURI(keyword)}</h3>
-                <ListOfGifs gifs={gifs} />
-            </>
-        } 
-        <button onClick={handleNextPage}>Get next page</button>
+  // Test para handleNextPage del INFINITE SCROLL
+  // const handleNextPage = () => console.log('🐤', 'next page');
+
+  // handleNextPage w/ REF
+  // const debounceHandleNextPage = useRef();
+
+  // handleNextPage w/ DEBOUNCE + useCALLBACK
+  const debounceHandleNextPage = useCallback(
+    debounce(() => setPage((prevPage) => prevPage + 1), 200),
+    []
+  );
+
+  useEffect(
+    function () {
+      // console.log('🐤', isNearScreen);
+      if (isNearScreen) debounceHandleNextPage();
+    },
+    [debounceHandleNextPage, isNearScreen]
+  );
+
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h3 className="App-title">{decodeURI(keyword)}</h3>
+          <ListOfGifs gifs={gifs} />
+          <div id="visor" ref={externalRef}></div>
+        </>
+      )}
+      {/* <button onClick={handleNextPage}>Get next page</button> */}
     </>
+  );
 }
-
-
-
